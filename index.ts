e3cfccc3208f60b4aac2eb85283aa6dff257c6e4
GoitseoneKau <66080430+GoitseoneKau@users.noreply.github.com>
2024-10-19 21:11:09 +0200

    update build to tsc

--- index.ts
+++ index.ts
@@ -1,28 +1,29 @@
+
 //created in cmd by typing 'type NUL > index.js'
 //insert "type":"module" in package.json in order to use imports
 import express from 'express';
 import cors from 'cors'
-import api_routes from './routes/api_routes.js'
+import api_routes from './src/routes/api_routes.js'
 import path from 'path'
 import { fileURLToPath } from 'url';
 
 
-const PORT =process.env.PORT || 8080;
+const PORT =process.env.PORT || 8000;
 const app = express();
 
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
-
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use(cors())
 
 app.use("/api",api_routes);
+app.get("/",(req,res)=>{
+res.send(__dirname)
+})
 
-app.use("/",api_routes,(request,response)=>{
-    response.sendFile(__dirname+"/index.html")
-});
 
 
 
