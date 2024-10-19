66aa646985df4e2749ec026dd1185597718a370c
GoitseoneKau <66080430+GoitseoneKau@users.noreply.github.com>
2024-10-19 16:33:21 +0200

    change file name users.ts to api_routes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 //insert "type":"module" in package.json in order to use imports
 import express, { Request, Response } from 'express';
 import cors from 'cors'
-import userRoutes from './routes/users.js'
+import apiRoutes from './routes/api_routes.js'
 
 
 const PORT =process.env.PORT|| 3001;
@@ -10,7 +10,7 @@ const app = express()
 
 
 app.use(cors())
-app.use("/api",userRoutes);
+app.use("/api",apiRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
