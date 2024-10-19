cdc1cd7fc892e43710d89f6a0b062f064955341c
GoitseoneKau <66080430+GoitseoneKau@users.noreply.github.com>
2024-10-19 18:41:28 +0200

    add index.html

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
 //created in cmd by typing 'type NUL > index.js'
 //insert "type":"module" in package.json in order to use imports
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors'
 import api_routes from './routes/api_routes.js'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const PORT =process.env.PORT || 8080;
+const app = express();
 
 
-const PORT =process.env.PORT|| 3001;
-const app = express()
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 
 app.use(cors())
+
 app.use("/api",api_routes);
+
+app.use("/",api_routes,(request,response)=>{
+    response.sendFile(__dirname+"/index.html")
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
