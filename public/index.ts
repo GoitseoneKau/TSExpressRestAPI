95a8668ef07346f2cd381c5f9bd97428517404aa
GoitseoneKau <66080430+GoitseoneKau@users.noreply.github.com>
2024-10-19 19:19:02 +0200

    adjust folders

--- public/index.ts
+++ public/index.ts
@@ -6,6 +6,7 @@ import api_routes from './routes/api_routes.js'
 import path from 'path'
 import { fileURLToPath } from 'url';
 
+
 const PORT =process.env.PORT || 8080;
 const app = express();
 
@@ -14,11 +15,15 @@ const __filename = fileURLToPath(import.meta.url); // get the resolved path to t
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 
+
 app.use(cors())
 
 app.use("/api",api_routes);
 
-//  
+app.use("/",api_routes,(request,response)=>{
+    response.sendFile(__dirname+"/index.html")
+});
+
 
 
 app.listen(PORT, () => {
