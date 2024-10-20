8f750f225c02c71bb04febd33c0a08d3187736b3
GoitseoneKau <66080430+GoitseoneKau@users.noreply.github.com>
2024-10-20 20:43:54 +0200

    built app

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+//created in cmd by typing 'type NUL > index.js'
+//insert "type":"module" in package.json in order to use imports
+import express from 'express';
+import cors from 'cors';
+import api_routes from './src/routes/api_routes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const PORT = process.env.PORT || 8000;
+const app = express();
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use("/api", api_routes);
+app.get("/", (req, res) => {
+    res.send(__dirname);
+});
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
---
+++ dist/src/routes/api_routes.js
@@ -0,0 +1,111 @@
+import express from "express";
+import users from '../assets/users/users_list.json' with { type: "json" };
+import todos from '../assets/todos/todos_list.json' with { type: "json" };
+const router = express.Router();
+router.use(express.json());
+//CRUD functions for Posts
+//get all
+router.get('/users', (request, response) => {
+    response.json(users.users); //response to client, in json
+});
+//get one user
+router.get('/users/:id', (request, response) => {
+    const id = parseInt(request.params.id);
+    response.json(users.users.find((user) => user.id === id)); //response to client, in json
+});
+//post a new user
+router.post('/users', (request, response) => {
+    var _a;
+    console.log("post requested");
+    let user = request.body.user;
+    user.id = ((_a = users.users) === null || _a === void 0 ? void 0 : _a.length) + 1;
+    if (user) {
+        users.users.push(user);
+        response.json(users); //201 'Created' - Indicates that the request has succeeded and a new resource has been created as a result.
+    }
+});
+//update a user
+router.put('/users/:id', (request, response) => {
+    let user = request.body;
+    let id = parseInt(request.params.id);
+    if (user) {
+        const userToUpdate = users.users.find((user) => user.id === id);
+        if (userToUpdate) {
+            userToUpdate.email = user.email;
+            userToUpdate.firstName = user.firstName;
+            userToUpdate.lastName = user.lastName;
+            userToUpdate.password = user.password;
+            userToUpdate.phone = user.phone;
+            response.json(users.users); //201 'Created' - Indicates that the request has succeeded and a new resource has been created as a result.
+        }
+        else {
+            response.json({ message: "user was not found" });
+        }
+    }
+});
+//delete a user
+router.delete('/users/:id', (request, response) => {
+    let user = request.body;
+    let id = parseInt(request.params.id);
+    if (user) {
+        const userToDelete = users.users.find((user) => user.id === id);
+        if (userToDelete) {
+            const newList = users.users.filter((user) => user.id !== id);
+            users.users = newList;
+            response.json(newList); //201 'Created' - Indicates that the request has succeeded and a new resource has been created as a result.
+        }
+        else {
+            response.json({ message: "user was not found" });
+        }
+    }
+});
+//CRUD functions for Todos
+//get all todos
+router.get('/todos', (request, response) => {
+    response.json(todos.todos); //response to client, in json
+});
+//get one todo
+router.get('/todos/:id', (request, response) => {
+    let id = parseInt(request.params.id);
+    response.json(todos.todos.find((todo) => todo.id === id)); //201 'Created' - Indicates that the request has succeeded and a new resource has been created as a result.
+});
+//post a new todo
+router.post('/todos', (request, response) => {
+    let todo = request.body;
+    todos.todos.push(todo);
+    response.json(todos.todos); //201 'Created' - Indicates that the request has succeeded and a new resource has been created as a result.
+});
+//edit todo
+router.put('/todos/:id', (request, response) => {
+    let todo = request.body;
+    let id = parseInt(request.params.id);
+    const todoToUpdate = todos.todos.find((user) => user.id === id);
+    if (todoToUpdate) {
+        todoToUpdate.todo = todo.todo;
+        todoToUpdate.priority = todo.priority;
+        todoToUpdate.priorityColor = todo.priorityColor;
+        todoToUpdate.dueDate = todo.dueDate;
+        todoToUpdate.completed = todo.completed;
+        response.json(todos.todos); //201 'Created' - Indicates that the request has succeeded and a new resource has been created as a result.
+    }
+    else {
+        response.json({ message: " was not found" });
+    }
+});
+//delete todo
+router.delete('/todos/:id', (request, response) => {
+    let user = request.body;
+    let id = parseInt(request.params.id);
+    if (user) {
+        const userToDelete = todos.todos.find((user) => user.id === id);
+        if (userToDelete) {
+            const newList = todos.todos.filter((user) => user.id !== id);
+            todos.todos = newList;
+            response.json(newList); //201 'Created' - Indicates that the request has succeeded and a new resource has been created as a result.
+        }
+        else {
+            response.json({ message: "user was not found" });
+        }
+    }
+});
+export default router;
